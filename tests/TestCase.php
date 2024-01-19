743c25bd74f4fbd134d4d5e9a589f4f30a20043b
Roel <10514742+rboonzaijer@users.noreply.github.com>
2024-01-19 21:21:42 +0100

    fix error: trait not found

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,14 +2,11 @@
 
 namespace RBoonzaijer\LaravelMultipleFlashMessages\Tests;
 
-use Orchestra\Testbench\Concerns\WithWorkbench;
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
 use RBoonzaijer\LaravelMultipleFlashMessages\FlashMessageServiceProvider;
 
 class TestCase extends OrchestraTestCase
 {
-    use WithWorkbench;
-    
     protected function getPackageProviders($app)
     {
         return [FlashMessageServiceProvider::class];
